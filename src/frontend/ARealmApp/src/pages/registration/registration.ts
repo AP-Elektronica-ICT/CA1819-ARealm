94b8f6046187c8b3f061743fd5a114565067c5e1
RobbeV <robbe.vermeire@student.ap.be>
2018-10-23 12:20:57 +0200

    Add navigation button to PuzzleTask

--- src/frontend/ARealmApp/src/pages/registration/registration.ts
+++ src/frontend/ARealmApp/src/pages/registration/registration.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { MapPage } from '../map/map';
 import { SetupPage } from '../setup/setup';
+import { PuzzleTaskPage } from '../puzzle-task/puzzle-task';
 
 /**
  * Generated class for the RegistrationPage page.
@@ -31,6 +32,9 @@ export class RegistrationPage {
 
   gotoMap(){
     this.navCtrl.push(MapPage);
+  }
+  gotoPuzzleTask(){
+    this.navCtrl.push(PuzzleTaskPage);
   }
 
 }
